7b252a64dd0eab436311150a687a2082f9426e76
B Kleinen <kleinen@infrastructure.de>
2017-12-15 11:10:40 +0100

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129075440) do
+ActiveRecord::Schema.define(version: 20171215100847) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
 
   create_table "pumpkins", force: :cascade do |t|
     t.string "species"
