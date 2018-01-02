f00761462dda73c54682fd1d8f763b8a7943a83a
B Kleinen <kleinen@infrastructure.de>
2018-01-02 14:16:43 +0100

    seed data for order with line_items

--- db/seeds/customer_with_order.rb
+++ db/seeds/customer_with_order.rb
@@ -1,2 +1,7 @@
-c1 = Customer.create(name: 'Ada Lovelace')
-c1.orders.create
+c = Customer.create(name: 'Grace Hopper')
+
+p1 = Pumpkin.create(species: 'Hokkaido', price: 2)
+p2 = Pumpkin.create(species: 'Butternut', price: 4)
+o = c.orders.create
+o.line_items.create(pumpkin: p1, amount: 4)
+o.line_items.create(pumpkin: p2, amount: 3)
