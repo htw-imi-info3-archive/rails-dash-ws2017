ebd85cc95c37b4b899ccbf4bf7deb5f6c97fff64
maxlinke <maxlinke@noreply.github.com>
2018-01-09 18:14:59 +0100

    shit works yo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :orders
+  
+  def total
+	sum = 0
+	orders.each do |o|
+		sum = sum + o.total
+	end
+	return sum
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ApplicationRecord
   has_many :line_items
   has_many :pumpkins, through: :line_items
   # has_many(:pumpkins, {through: :line_items})
+  
+  
+  def total
+	sum = 0
+	line_items.each do |li|
+		sum = sum + (li.pumpkin.price * li.amount)
+	end
+	sum = sum
+  end
+  
 end
