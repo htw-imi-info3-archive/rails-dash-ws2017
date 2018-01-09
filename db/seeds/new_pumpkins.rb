b3f8ac449663167a8c3d2fb71d4f969a9a5b40f6
Linda Tram <lintram98@gmail.com>
2018-01-09 14:38:42 +0100

    riesenzentner added

--- db/seeds/new_pumpkins.rb
+++ db/seeds/new_pumpkins.rb
@@ -1,1 +1,2 @@
 pump = Pumpkin.create(species: 'Tuerkenturban', carved: true, size: 2, weight: 8, price: 21)
+pump2 = Pumpkin.create(species: 'Riesenzentner', carved: false, size: 4, weight: 10, price: 30)
