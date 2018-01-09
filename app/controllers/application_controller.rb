22aef87fe951cac30c111384802f9d237497ca65
B Kleinen <kleinen@infrastructure.de>
2018-01-09 13:48:02 +0100

    pass the locale in every helper generated url, simplest locale switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  before_action :set_i18n_locale_from_params
 
+  before_action :set_i18n_locale_from_params
+  def default_url_options
+    { locale: I18n.locale }
+  end
   protected
     def set_i18n_locale_from_params
       if params[:locale]
@@ -15,4 +17,5 @@ class ApplicationController < ActionController::Base
         end
       end
     end
+
 end
