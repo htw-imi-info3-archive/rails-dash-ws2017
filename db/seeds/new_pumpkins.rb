814b6e9edfbb93ae93aad08100be49d96f1f77e0
Antonia Rohne <antonia.rohne@web.de>
2018-01-09 14:31:40 +0100

    delete pumkin seed 2

--- db/seeds/new_pumpkins.rb
+++
@@ -1,2 +0,0 @@
-pump = Pumpkin.create(species: 'Tuerkenturban', carved: false, size: 5, weight: 3, price: 10)
-p1 = Pumpkin.create(species: 'Riesenzentner', carved: true, size: 4, weight: 8, price: 20)
