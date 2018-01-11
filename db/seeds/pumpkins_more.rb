cc35963b4c54a417e481e977f1bff5937fa9e39c
J. Busch <josobu33@gmail.com>
2018-01-11 17:42:59 +0100

    more pumpkins by LuSaJo

---
+++ db/seeds/pumpkins_more.rb
@@ -0,0 +1,4 @@
+Pumpkin.create(species: 'Muscovado', carved: false, size: 3, weight: 3, price: 15)
+Pumpkin.create(species: 'Olga Squash', carved: false, size: 2, weight: 3, price: 17)
+Pumpkin.create(species: 'Atlantic giant', carved: false, size: 20, weight: 15, price: 17)
+ 
