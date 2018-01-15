7adb56e74fb93da09a6b1a382de5c18cc4a11849
0LAG0 <lagaly.j@googlemail.com>
2018-01-15 21:32:29 +0100

    Merge Servant für Aufgabe 6 (mit gitignore :-)) (#24)

---
+++ db/seeds/pumpkin_rgb.rb
@@ -0,0 +1,5 @@
+p1 = Pumpkin.create(species: 'billy the pumpkin', carved: false, size: 3234, weight: 632, price: 32239)
+p2 = Pumpkin.create(species: 'jonny the pumpkin', carved: true, size: 32, weight: 35, price: 234)
+
+c1 = Customer.create(name: 'Anh Quang Vu Tuyen')
+c2 = Customer.create(name: 'Jakob Lagaly')
