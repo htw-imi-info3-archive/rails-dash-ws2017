0f0dfb9a48518bcd4624028965f6859a214d942e
0LAG0 <lagaly.j@googlemail.com>
2018-01-15 20:17:50 +0100

    fin aufgabe 6

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,13 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :line_items
   has_many :pumpkins, through: :line_items
-  # has_many(:pumpkins, {through: :line_items})
+
+  def Sum
+    total = 0
+    line_items.each do |item|
+      total = total + item.price
+    end
+    return total
+  end
+
 end
---
+++ db/seeds/pumpkin_sangaly.rb
@@ -0,0 +1,3 @@
+p1 = Pumpkin.create(species: 'magnifiza', carved: false, size: 1, weight: 3, price: 60)
+p2 = Pumpkin.create(species: 'sangsua', carved: true, size: 10, weight: 45, price: 40)
+p3 = Pumpkin.create(species: 'herbert the living pumpkin', carved: true, size: 56, weight: 90, price: 60000)
