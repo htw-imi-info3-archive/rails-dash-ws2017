be11dfbacddc1632963dd69fc77eb1f49aa420c1
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2018-01-19 22:22:20 +0100

    ..

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,3 @@
 class Comment < ApplicationRecord
   belongs_to :pumpkin
-
- def pumpkin_species_to_param
- return Pumpkin.species
- end
- def self.search(search)
- if search
-  where("content LIKE ?", "%#{search}%")
-else
-all
- end
- end
 end
