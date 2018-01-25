76830f617d36f1a5cd73a6aaeee4052fa3f5f85c
tony-dorfmeister <tony.dorfmeister@me.com>
2018-01-25 12:01:01 +0100

    ex01: sorting pumpkins:index by price ASC
    issue here:
    https://github.com/htw-imi-info3/rails-dash-ws2017/issues/71

--- app/controllers/pumpkins_controller.rb
+++ app/controllers/pumpkins_controller.rb
@@ -4,7 +4,7 @@ class PumpkinsController < ApplicationController
   # GET /pumpkins
   # GET /pumpkins.json
   def index
-    @pumpkins = Pumpkin.all
+    @pumpkins = Pumpkin.order(price: :asc)
   end
 
   # GET /pumpkins/1
